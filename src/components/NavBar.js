c47833e2bb4012fa97ad1d060f0d526db987f991
Alice <aliceliu2003@yahoo.com>
2024-09-25 20:12:52 -0400

    Updated nav bar
    changed padding between nav bar text, made button look more porportional

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,11 +8,11 @@ import './NavBar.css';
 export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-       <AppBar position="static" sx={{ boxShadow: 'none' }}>
+      <AppBar position="static" sx={{ boxShadow: 'none' }}>
         <Toolbar sx={{ justifyContent: 'flex-end' }}>
-          <Button color="inherit">Home</Button>
-          <Button color="inherit">Courses</Button>
-          <Button className="create-course-button"> {/* Add a class for styling */}
+          <Button color="inherit" sx={{ padding: '20px' }}>Home</Button>
+          <Button color="inherit" sx={{ padding: '20px' }}>Courses</Button>
+          <Button className="create-course-button" sx={{ padding: '8px 12px' }}>
             Create Course Listing
           </Button>
         </Toolbar>
