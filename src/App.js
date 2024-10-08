a012afbcb1f3123f58bb461c1c3adbeb79078aaa
Emmanuel Makoye <makoye@Emmanuels-MacBook-Air.local>
2024-10-08 13:43:00 -0400

    updated App.js to remove api example usage

--- src/App.js
+++ src/App.js
@@ -49,28 +49,6 @@ function App() {
     }
   }
 
-  // Fetch the courses from the API and log them
-  useEffect(() => {
-    // Fetch the courses when the component mounts
-    const fetchCourses = async () => {
-      const course = {
-        "courseId": "makoye",
-        "title": "Introduction to Software Engineering",
-        "description": "This course covers the fundamental concepts of software engineering, including design patterns, development methodologies, and project management.",
-        "createdBy": "professor123",
-        "aliases": ["SE101", "SoftwareEng"],
-        "prerequisites": ["CS101", "CS102"],
-        "reviews": []
-      }
-      const res = await createCourse(course);
-      console.log(res);
-    };
-
-    fetchCourses().then(() => console.log("Courses fetched"));
-  }, []);
-
-
-
   return (
     <div className="App">
       <PageContext.Provider value={{ page, setPage }} >
